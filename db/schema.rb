f834d4fd5237cd6b16eee5232fb8954321c0ab86
Henry Orosco <horosco@gmail.com>
2015-04-19 23:45:00 -0500

    adding modifications to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312032111) do
+ActiveRecord::Schema.define(version: 20150414042231) do
+
+  create_table "cpus", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "freq"
+    t.integer  "cores"
+    t.string   "L1_cache"
+    t.string   "L2_cache"
+    t.string   "chipset"
+    t.string   "overclockable"
+    t.string   "power_needed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "drams", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "type"
+    t.string   "size"
+    t.string   "freq"
+    t.string   "transfer_rate"
+    t.string   "for_server_or_pc"
+    t.string   "power_needed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "gpus", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "cuda_cores"
+    t.string   "dram"
+    t.string   "freq"
+    t.string   "connection"
+    t.string   "power_needed"
+    t.integer  "supported_displays"
+    t.string   "flops"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hdds", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "size"
+    t.string   "freq"
+    t.string   "transfer_rate"
+    t.string   "r_w_rate"
+    t.string   "connection"
+    t.string   "power_needed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "motherboards", force: true do |t|
     t.integer  "price"
@@ -29,6 +87,57 @@ ActiveRecord::Schema.define(version: 20150312032111) do
     t.string   "pci_ports"
     t.string   "pcie_ports"
     t.string   "power_needed"
+    t.string   "dimensions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "channel_supported"
+    t.integer  "pcie3x16_ports"
+    t.integer  "pcie2x16_ports"
+    t.integer  "mini_card_slots"
+    t.integer  "sata_exp_ports"
+    t.integer  "m2_ports"
+    t.string   "lan_chipset"
+    t.string   "second_lan_chipset"
+    t.string   "max_lan_speed"
+    t.integer  "ps2_ports"
+    t.integer  "dvi_i_ports"
+    t.integer  "hdmi_ports"
+    t.integer  "display_ports"
+    t.integer  "esata_ports"
+    t.integer  "s_pdif_out_ports"
+    t.integer  "audio_ports"
+    t.string   "other_internal_ports"
+  end
+
+  create_table "powersupplies", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "power"
+    t.string   "rails"
+    t.string   "connection"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ssds", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
+    t.string   "size"
+    t.string   "freq"
+    t.string   "transfer_rate"
+    t.string   "r_w_rate"
+    t.string   "connection"
+    t.string   "power_needed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "towers", force: true do |t|
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "model"
     t.string   "dimensions"
     t.datetime "created_at"
     t.datetime "updated_at"
